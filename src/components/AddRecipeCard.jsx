e36202ffcad5856a183f1b300dbffe6c9871afe1
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-07-01 22:17:07 +0530

    update: added useEffect

--- src/components/AddRecipeCard.jsx
+++ src/components/AddRecipeCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRecipeContext } from "../context/RecipeContext";
 export const AddRecipeCard = ({ setIsModalOpen }) => {
   const { addRecipe } = useRecipeContext();
